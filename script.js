bf38e02a58f4ff1e348ec7b044a3ee29234f9f46
ramsayg <ramsaygonzalez@outlook.com>
2020-03-16 23:25:42 -0400

    Updated js file to create desired variables and capture user inputs which creates a string of password characters to use.

--- script.js
+++ script.js
@@ -1,29 +1,66 @@
-var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialChar = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-var pwLength = prompt("How many characters?");
-var useLowercase = confirm("Would you like your password to contain a lowercase letter?");
-var useUppercase = confirm("Would you like your password to contain a upercase letter?");
-var useNumber = confirm("Would you like your password to contain a number?");
-var useSpecialChar = confirm("Would you like your password to contain a special character?");
-var userResponses = [useLowercase, useUppercase, useNumber, useSpecialChar];
-
-console.log(userResponses.some(Boolean));
+var arrLowerCase = Array.from(lowerCase);
+var arrUpperCase = Array.from(upperCase);
+var arrNumbers = Array.from(numbers);
+var arrSpecialChar = Array.from(specialChar);
 
 
+function generatePassword() {
+  var pwLength = prompt("Enter desired password length. Must be greater than or equal to 8 but less than or equal to 128?");
+  if (pwLength >= 8 && pwLength <= 128) {
+    var useLowercase = confirm("Would you like your password to contain a lowercase letter?")
+    var useUppercase = confirm("Would you like your password to contain a upercase letter?");
+    var useNumber = confirm("Would you like your password to contain a number?");
+    var useSpecialChar = confirm("Would you like your password to contain a special character?");
+    var arrUserResponses = [useLowercase, useUppercase, useNumber, useSpecialChar]
+    var arrPasswordCharacters = [];
 
+    if (arrUserResponses.some(Boolean)) {
+      if (arrUserResponses[0] === true) {
+        arrPasswordCharacters.push(arrLowerCase)
+      }
+      if (arrUserResponses[1] === true) {
+        arrPasswordCharacters.push(arrUpperCase)
+      }
 
+      if (arrUserResponses[2] === true) {
+        arrPasswordCharacters.push(arrNumbers)
+      }
 
-
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+      if (arrUserResponses[3] === true) {
+        arrPasswordCharacters.push(arrSpecialChar)
+      }
+      var arrMergePassChar = arrPasswordCharacters.flat(1)
+      var PasswordCharacters = arrMergePassChar.toString()
+    
+      console.log(arrPasswordCharacters)
+      console.log(arrMergePassChar)
+      console.log(PasswordCharacters)
+      console.log("good")
+    }
+    else {
+      console.log("bad")
+    }
+  }
+  else {
+    console.log("Bad")
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generatePassword()
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
