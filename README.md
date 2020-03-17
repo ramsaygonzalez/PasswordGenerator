7948a976291a4cbe326ebbc1406c0a6cfcbc60c4
ramsayg <ramsaygonzalez@outlook.com>
2020-03-17 00:49:00 -0400

    Updated read me file.  Updated JS file to enable the application to generate and display a random password in the browser.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PasswordGenerator
 Homework # 3
+
+Goal was to create an application that generates a random password based on user selected criteria.  At this point I have been able to achomplish this.  The user interface has not been updated or its responsiveness to multiple screen sizes.  This will be worked on as soon as possible
+
+The application uses user "prompt" to gather information required to determine the length of the password.  It also uses "confirms" to identify the desired character types to use for the password.  "If" Statements were used to identify if the desired parameters required from the user were met.  If not the user is notified by an "Alert" and is then directed to the begining of the series of prompts and confirms.
+
--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialChar = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+// Converting the required carracters into elements in different arrays for later use
 var arrLowerCase = Array.from(lowerCase);
 var arrUpperCase = Array.from(upperCase);
 var arrNumbers = Array.from(numbers);
 var arrSpecialChar = Array.from(specialChar);
 
-
+// Code to generate random password
 function generatePassword() {
+  // Gathering user inputs for required password criteria
   var pwLength = prompt("Enter desired password length. Must be greater than or equal to 8 but less than or equal to 128?");
   if (pwLength >= 8 && pwLength <= 128) {
     var useLowercase = confirm("Would you like your password to contain a lowercase letter?")
     var useUppercase = confirm("Would you like your password to contain a upercase letter?");
     var useNumber = confirm("Would you like your password to contain a number?");
     var useSpecialChar = confirm("Would you like your password to contain a special character?");
+    // This array will be used to ensure at least one character type is selected for password generation
     var arrUserResponses = [useLowercase, useUppercase, useNumber, useSpecialChar];
+    // Based on the users response this array will store the characters that will be used to generate the password
     var arrPasswordCharacters = [];
-
+    // Checks to see if at least one response is true to ensure at least one character type has been selected
     if (arrUserResponses.some(Boolean)) {
       if (arrUserResponses[0] === true) {
         arrPasswordCharacters.push(arrLowerCase);
@@ -33,41 +39,37 @@ function generatePassword() {
       if (arrUserResponses[3] === true) {
         arrPasswordCharacters.push(arrSpecialChar);
       }
+      // Code above stores the data in multiple arrays this line of code combines into a single array
       var arrMergePassChar = arrPasswordCharacters.flat(1);
+      // This array will store the final value which will be presented on the broswer
       var autoPassword = []
-
+      // Used to generate the random password and stor in array above
       for (let i = 0; i < pwLength; i++) {
         var randomPassword = arrMergePassChar[~~(Math.random() * arrMergePassChar.length)];
         autoPassword.push(randomPassword)
       }
-
-      console.log(arrPasswordCharacters)
-      console.log(arrMergePassChar)
-      console.log(autoPassword.join(""))
-      console.log("good")
+      // Returns a string which will be presented as the Password in the browser
+      return autoPassword.join("")
     }
     else {
+      // Informs user that they did not select at least one character type and starts the function over again
       alert("Please select at least one character type.  Thanks!")
-      console.log("bad")
+      generatePassword()
     }
   }
   else {
-    alert("Please select a password length greater than or equal to 8 or less than or equal to 128.  Thanks!")
-    console.log("Bad")
+    // informs user that they did not select the approriate character length and starts the function over again
+    alert("Please select a password length greater than or equal to 8 and less than or equal to 128.  Thanks!")
+    generatePassword()
   }
 }
 
-generatePassword()
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
